cbc8828c5e7f2d40bab8f7f9cb16928801f520e4
Avi Romanoff <avi@romanoff.me>
2016-12-02 01:55:25 -0500

    it's happening :^)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import classNames from 'classnames'
 
 const MAX_HEIGHT = 250
 const MAX_BYTES = 5242880 // 5 megabytes
+const SQUARE_FORGIVENESS = 0.05
 
 class App extends Component {
   render() {
@@ -15,6 +16,9 @@ class App extends Component {
       <div className="App">
         <img className="Logo" src={logotype}/>
         <UploadZone/>
+        <div className="Footer">
+        <a href="http://avi.bio" target="_blank">built & designed by Avi</a>
+        </div>
       </div>
     );
   }
@@ -23,14 +27,10 @@ class App extends Component {
 function loadImage(src, onload) {
     let img = new Image();
     img.src = src;
-    img.onload = () => {
-      console.log("onload!")
-      onload(img)
-    }
+    img.onload = () => onload(img)
 }
 
-function murder(profile, banner) {
-  console.log('murder')
+function applyMurderFilter(profile, banner) {
   const canvas = document.createElement('canvas')
   const ctx = canvas.getContext('2d')
   canvas.width = 500
@@ -42,67 +42,53 @@ function murder(profile, banner) {
   const imageData = ctx.getImageData(0, 0, profile.width, profile.height);
   const data = imageData.data;
 
+  // Desaturate
   for (let i = 0; i < data.length; i += 4) {
-    // const brightness =
-    const brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
-    // red
-    data[i] = brightness;
-    // green
-    data[i + 1] = brightness;
-    // blue
-    data[i + 2] = brightness;
+    const saturation = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
+    data[i] = saturation;
+    data[i + 1] = saturation;
+    data[i + 2] = saturation;
   }
 
-  const contrast = 125
+  // Increase contrast
+  const contrast = +125
   const factor = (259 * (contrast + 255)) / (255 * (259 - contrast));
-  for (var i=0;i<data.length;i+=4) {
+  for (let i = 0; i < data.length; i += 4) {
     data[i] = factor * (data[i] - 128) + 128;
     data[i+1] = factor * (data[i+1] - 128) + 128;
     data[i+2] = factor * (data[i+2] - 128) + 128;
   }
 
+  // Increase brightness
+  const brightness = +20
   for (let i = 0; i < data.length; i += 4) {
-    const delta = +10
-    // const brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
-    // red
-    data[i] += delta;
-    // green
-    data[i + 1] += delta;
-    // blue
-    data[i + 2] += delta;
+    data[i] += brightness;
+    data[i + 1] += brightness;
+    data[i + 2] += brightness;
   }
 
-//   brightness = function(delta) {
-//     return function (pixels, args) {
-//       var d = pixels.data;
-//       for (var i = 0; i < d.length; i += 4) {
-//         d[i] += delta;     // red
-//         d[i + 1] += delta; // green
-//         d[i + 2] += delta; // blue
-//       }
-//       return pixels;
-//     };
-// };
-
-  // overwrite original image
   ctx.putImageData(imageData, 0, 0);
-
   ctx.drawImage(banner, 0, 0, banner.width, banner.height,
                 0, canvas.height - banner.height, banner.width, banner.height)
-  // const imageData = ctx.getImageData(0,0,canvas.width, canvas.height)
-  // const data = imageData.data
-  //
-  // const grayscale = function() {
-  //   for (let i = 0; i < data.length; i += 4) {
-  //     const avg = (data[i] + data[i +1] + data[i +2]) / 3;
-  //     data[i]     = avg; // red
-  //     data[i + 1] = avg; // green
-  //     data[i + 2] = avg; // blue
-  //   }
-  //   ctx.putImageData(imageData, 0, 0);
-  // };
 
   return canvas.toDataURL('image/png')
+}
+
+class Status extends Component {
+
+  render() {
+    <div className="Uploader-status">
+      {
+        !this.props.text
+        ?
+          <div>
+          </div>
+        :
+          <div>{this.props.text}</div>
+      }
+    </div>
+  }
+
 }
 
 class UploadZone extends Component {
@@ -114,8 +100,14 @@ class UploadZone extends Component {
       width: 0,
       height: 0,
       isShaking: false,
-      status: null,
+      status: 'Drag and drop profile picture below. Or click the circle.',
     }
+  }
+
+  setStatus(status) {
+    this.setState({
+      status,
+    })
   }
 
   shake() {
@@ -124,27 +116,27 @@ class UploadZone extends Component {
     setTimeout(() => this.setState({ isShaking: false }), 820)
   }
 
+  onDownload() {
+
+  }
+
   onDrop(files) {
     if (files.length !== 1) {
       this.shake()
+      this.setStatus('hey, this thing only accepts images — one at a time!')
       return
     }
 
     loadImage(files[0].preview, profile => {
-
-      if (profile.height != profile.width) {
+      if (Math.abs(1 - (profile.height / profile.width)) > SQUARE_FORGIVENESS) {
         this.shake()
-        this.setState({
-          status: 'image must gotta be a square, yo. try again?',
-        })
+        this.setStatus('the image has to be square-ish dimensions, sorry. try again?')
         return
       }
 
       if (profile.height < 200) {
         this.shake()
-        this.setState({
-          status: 'what is this, a profile picture for ants?',
-        })
+        this.setStatus('what the hell is this, a profile picture for ants!?')
         return
       }
 
@@ -162,12 +154,12 @@ class UploadZone extends Component {
       }
 
       loadImage(banner, bannerImg => {
-      console.log('wha2t')
         this.setState({
           width,
           height,
           file: files[0],
-          out: murder(profile, bannerImg),
+          out: applyMurderFilter(profile, bannerImg),
+          status: 'download',
         })
       })
     })
@@ -188,18 +180,16 @@ class UploadZone extends Component {
     }
     return (
       <div className="Uploader">
-        <div className="Uploader-status">
-          {
-            !this.state.status
-            ?
-              <div>
-                Drag and drop profile picture below.
-                <br/>
-                Or click the circle.
-              </div>
-            : <div>{this.state.status}</div>
-          }
-        </div>
+        {
+          this.state.file ? null :
+          <div className="Uploader-status">{this.state.status}</div>
+        }
+        {
+          !this.state.file ? null :
+          <a href={this.state.out} download="murderballad-profile.png">
+            <img className='Download' onClick={this.onDownload.bind(this)} src={download} />
+          </a>
+        }
         <Dropzone
           maxSize={MAX_BYTES}
           accept={'image/*'}
@@ -210,19 +200,7 @@ class UploadZone extends Component {
             (this.state.out == null) ? null :
             <img className='Dropped-image' src={this.state.out} />
           }
-          {/* {
-            (this.state.file == null) ? null :
-            <img className='Dropped-image' src={shopped} />
-          } */}
         </Dropzone>
-        {/* {
-          (this.state.out == null) ? null :
-          <img className='' src={this.state.out} />
-        } */}
-        {
-          (this.state.file == null) ? null :
-          <img className='Download' src={download} />
-        }
       </div>
     );
   }
